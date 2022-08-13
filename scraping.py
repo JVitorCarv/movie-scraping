33f94666f92bd6a91ae791e858f9cc49e18baafd
Vitor <vitor.velozo2003@gmail.com>
2022-08-12 23:53:34 -0300

    Print movie titles

--- scraping.py
+++ scraping.py
@@ -1,8 +1,16 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 
-year = 2021
-driver.get(f'https://www.metacritic.com/browse/movies/score/metascore/year/filtered?year_selected={year}&sort=desc')
+page = 0
+driver.get(f'https://www.metacritic.com/browse/movies/score/metascore/all/filtered?sort=desc&page={page}')
 
+info_box = driver.find_elements(By.CLASS_NAME, 'clamp-summary-wrap')
+
+titles = []
+for box in info_box:
+    titles.append(box.find_element(By.TAG_NAME, 'h3'))
+for title in titles:
+    print(title.text)
 driver.quit()
