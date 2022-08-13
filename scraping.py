2ef3fa195c6134551dba0175aec7b2920fe1422f
Vitor <vitor.velozo2003@gmail.com>
2022-08-13 00:43:30 -0300

    Store movies as objects

--- scraping.py
+++ scraping.py
@@ -8,9 +8,22 @@ driver.get(f'https://www.metacritic.com/browse/movies/score/metascore/all/filter
 
 info_box = driver.find_elements(By.CLASS_NAME, 'clamp-summary-wrap')
 
-titles = []
+class Movie:
+    def __init__(self, title, score):
+        self.title = title
+        self.score = score
+
+movies = []
+
 for box in info_box:
-    titles.append(box.find_element(By.TAG_NAME, 'h3'))
-for title in titles:
-    print(title.text)
+    title = box.find_element(By.TAG_NAME, 'h3').text
+    score = box.find_element(By.CLASS_NAME, 'metascore_w').text
+    score = int(score)
+    
+    if score < 100:
+        break
+    movies.append(Movie(title, score))
+    
+for movie in movies:
+    print(movie.title, movie.score)
 driver.quit()
